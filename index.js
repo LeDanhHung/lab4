65a575ccab1b82f8f3f200141e7bee9be539ccf7
ledanhhung <hungldph11449@fpt.edu.vn>
2021-04-02 11:09:47 +0700

    asaasd

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var expressHbs = require('express-handlebars');
 
+app.use('/assets',express.static(__dirname+'/assets'))
 app.engine('handlebars', expressHbs({
     layoutsDir:__dirname + '/views/layouts',
     defaultLayout:'main'
@@ -10,9 +11,16 @@ app.engine('handlebars', expressHbs({
 app.set('view engine', 'handlebars')
 
 app.get('/', function(request,response){
-    var name = 'Danh Hung'
-    var arr = ['Hùng','Lê','Danh'];
-    response.render('home',{name:name,array:arr});
+    response.render('home');
+});
+app.get('/login', function (request, response) {
+    response.render('login');
+});
+app.get('/dangki', function (request, response) {
+    response.render('dangki');
+});
+app.get('/user', function (request, response) {
+    response.render('user');
 });
 app.post('/', function(request,response){
 

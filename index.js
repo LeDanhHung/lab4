78288cab002bcfbaa96cb8f6b4518612b847a668
ledanhhung <hungldph11449@fpt.edu.vn>
2021-04-02 20:53:59 +0700

    ádas

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 var express = require('express');
 var app = express();
 var expressHbs = require('express-handlebars');
-
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true }))
 app.use('/assets',express.static(__dirname+'/assets'))
 app.engine('handlebars', expressHbs({
     layoutsDir:__dirname + '/views/layouts',
@@ -14,6 +15,7 @@ app.get('/', function(request,response){
     response.render('home');
 });
 app.get('/login', function (request, response) {
+
     response.render('login');
 });
 app.get('/dangki', function (request, response) {
@@ -25,4 +27,13 @@ app.get('/user', function (request, response) {
 app.post('/', function(request,response){
 
 });
+app.post('/login', function (request, response) {
+    
+    response.redirect('/login');
+});
+app.post('/dangki', function (request, response) {
+   
+    response.redirect('/dangki');
+});
+
 app.listen(process.env.PORT || '3006')

c7fb9f968d973abfd7b50c39291f7406c7ab360c
ledanhhung <hungldph11449@fpt.edu.vn>
2021-04-17 15:34:40 +0700

    adas

--- index.js
+++ index.js
@@ -2,6 +2,61 @@
 var express = require('express');
 var app = express();
 var expressHbs = require('express-handlebars');
+var router = express.Router();
+const mongoose = require("mongoose");
+var multer = require("multer");
+var upload = multer({ dest: "uploads/" });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+var urlDb =
+    "mongodb+srv://admin:admin@cluster0.fcoai.mongodb.net/tinder?retryWrites=true&w=majority";
+mongoose.connect(urlDb, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+});
+const { Schema } = mongoose;
+const user = new Schema({
+    email:String,
+    password: String,
+    name: String,
+    date: String,
+    gioitinh: String,
+    gioithieu: String,
+    sothich: String,
+    avatars: String,
+});
+var connectUsers;
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+    console.log("kết nối thành công");
+    
+});
+router.get("/", function (req, res, next) {
+  res.render('home');
+});
+app.post("/insertUser", upload.single('avatars'), (req, res, next) => {
+    connectUsers = db.model("users", user);
+    connectUsers({
+        email: req.body.email,
+        password: req.body.password,
+        name: req.body.name,
+        date: req.body.date,
+        gioitinh:req.body.gioitinh,
+        sothich: req.body.sothich,
+        gioithieu:req.body.gioithieu,
+       // avatars:req.file.path,
+    }).save(function (err) {
+        if (err) {
+           console.log(err);
+        } else {
+            res.redirect("/");
+        }
+    });
+});
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true }))
 app.use('/assets',express.static(__dirname+'/assets'))
@@ -22,7 +77,17 @@ app.get('/dangki', function (request, response) {
     response.render('dangki');
 });
 app.get('/user', function (request, response) {
-    response.render('user');
+    var connectUsers = db.model("user", user);
+     connectUsers.find({}, function (err, users) {
+        if (err) {
+            console.log(err);
+        } else {
+            response.render('user', { users: users }, {
+                allowProtoMethodsByDefault: true,
+                allowProtoPropertiesByDefault: true
+            });
+        }
+    });
 });
 app.post('/', function(request,response){
 
@@ -35,5 +100,7 @@ app.post('/dangki', function (request, response) {
    
     response.redirect('/dangki');
 });
+ app.post('/insertUser', function (request, response) {
+     response.redirect('/'); });
 
-app.listen(process.env.PORT || '3006')
+app.listen(process.env.PORT || '3007')
